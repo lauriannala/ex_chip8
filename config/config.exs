b064cf79f22649910fc473c404bf8b28f967927d
Lauri Annala <lauri.annala@protonmail.com>
2021-11-30 18:41:52 +0200

    Sleep wait period

--- config/config.exs
+++ config/config.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :ex_chip8, :chip8_width, 64
 config :ex_chip8, :chip8_height, 32
-config :ex_chip8, :sleep_wait_period, 100
+config :ex_chip8, :sleep_wait_period, 2
 # config :ex_chip8, :sleep_wait_period, 1000
 config :ex_chip8, :chip8_memory_size, 4096
 config :ex_chip8, :chip8_total_data_registers, 16
